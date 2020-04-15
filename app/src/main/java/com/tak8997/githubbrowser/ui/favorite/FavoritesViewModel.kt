25e117223491c6c92cf553872a85db0abecc5c5c
kyle lee <kyle.lee@balancehero.com>
2020-04-15 18:07:06 +0900

    add inject annotation

--- app/src/main/java/com/tak8997/githubbrowser/ui/favorite/FavoritesViewModel.kt
+++ app/src/main/java/com/tak8997/githubbrowser/ui/favorite/FavoritesViewModel.kt
@@ -1,7 +1,8 @@
 package com.tak8997.githubbrowser.ui.favorite
 
 import com.tak8997.githubbrowser.BaseViewModel
+import javax.inject.Inject
 
-internal class FavoritesViewModel : BaseViewModel() {
+internal class FavoritesViewModel @Inject constructor() : BaseViewModel() {
 
 }
